16d640a46cdd8fece0bae36c022a8aa9f6193dbc
diloytd <khadieva.diana@yandex.ru>
2024-06-20 11:49:49 +0300

    pard_one

---
+++ src/Header/header.jsx
@@ -0,0 +1,14 @@
+
+function Header (){
+    return (
+        <div className='container'>
+            <div className="logIn"><button>Войти</button></div>
+            <div className="newWords">Новый слова</div>
+            <div className="card">Карточка</div>
+            <div className="listWord">Список слов</div>
+        </div>
+
+    )
+}
+
+export default Header

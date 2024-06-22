f8ccf20cb09c6e1517eb31fe2057e5b71c595b10
diloytd <khadieva.diana@yandex.ru>
2024-06-22 13:55:45 +0300

    part2

--- src/Header/header.jsx
+++ src/Header/header.jsx
@@ -1,13 +1,21 @@
-
+import styles from './header.module.scss'
+import '../Date/Fonts/Livvic-MediumItalic.ttf'
+import '../Date/colors.scss'
+import '../Date/mixins.scss'
 function Header (){
     return (
-        <div className='container'>
-            <div className="logIn"><button>Войти</button></div>
-            <div className="newWords">Новый слова</div>
-            <div className="card">Карточка</div>
-            <div className="listWord">Список слов</div>
+        <>
+        <header className={styles.header}>
+            <div className={styles.nav}>
+        <button className={styles.btn}>Войти</button>
+        <div className={styles.container}>
+            <div className="newWords"><a href='#'>Новые слова</a></div>
+            <div className="card"><a href='#'>Карточки</a></div>
+            <div className="listWord"><a href='#'>Список слов</a></div>
         </div>
-
+        </div>
+        </header>
+        </>
     )
 }
 
---
+++ src/Main/main.jsx
@@ -0,0 +1,13 @@
+import style from './main.module.scss'
+
+function Main (){
+    return(
+        <>
+            <div className={style.container}>
+
+            </div>
+        </>
+    )
+}
+
+export default Main
